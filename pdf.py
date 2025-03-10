5a3fb3e42ba27a746fa7202fff6211c80a70fbf0
RumazQureshi <rumazqureshi@gmail.com>
2025-03-10 21:33:35 +0500

    updated

--- pdf.py
+++ pdf.py
@@ -1,13 +1,9 @@
 import PyPDF2 #importing pypdf2 library for merging pdfs
 
-pdf_files = ["1.pdf","2nd.pdf","3rd.pdf"] #list of pdf files to be merged
+pdf_files = ["1st PDF.pdf","2nd PDF.pdf","3rd PDF.pdf"] #list of pdf files to be merged
 merger = PyPDF2.PdfMerger() #pdf merger object
 for pdfs in pdf_files: #for loop for merging all the pdfs in the list
     merger.append(pdfs) #append will pend the pdf size 
 merger.write("Merged.pdf") #it will create a new pdf named as merged.pdf
 merger.close()
 print('''PDFs MERGED SUCCESSFULLY INTO NEW PDF FILE "Merged.pdf" ''')
-
-import PyPDF2 #importing pypdf2 library for spiliting pdfs
-pdf_files = ["1.pdf","2nd.pdf","3rd.pdf"] #list of pdf files to be merged
-spiliter=PyPDF2.pagerange(1)
